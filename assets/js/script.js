e5221d3fe7ea4eb55a756605edfb0c6f1ce0fafc
Malachi <malachialusa@gmail.com>
2020-11-13 19:12:15 -0700

    Added choice branches for password selection, true and false statment selectors, and selection array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,157 @@
 // Assignment code here
+  
+document.getElementById("generate").addEventListener("click",function password(){
+
+  var lengthChoice =window.prompt("How Long would you like your password to be? Please enter number between 8-128 for your passwords length.")
+
+  if(typeof lengthChoice === "string"){// go and add this through your choices 
+
+    window.alert("that is a word not a number. Please try agin");
+
+    password();
+    return;
+  }
+
+  if(lengthChoice <= 7 || lengthChoice >= 129){
+
+    window.alert("Your Password length must be between 8 and 128 Characters. Please try again");
+
+    password();
+    return;
+  }
+
+  if(lengthChoice >= 8 && lengthChoice <= 128) {
+
+    var upCasecheck =window.prompt("Would you like to include uppercase letters? Y for Yes, N for No.")
 
 
+    upCasecheck= upCasecheck.toLowerCase()
+
+    if (upCasecheck ==="" || upCasecheck === null){
+
+      window.alert("That is not a  valid option. Please start again")
+
+      password()
+      return;}
+
+    if(upCasecheck === "y"){
+
+      uppercase=true}
+
+    if(upCasecheck === "n"){
+
+      uppercase=false;}
+      
+    var lowerCasecheck= window.prompt("Would you like to include lowercase letters? Y for yes N for No.")
+
+    lowerCasecheck= lowerCasecheck.toLowerCase()
+
+    if (lowerCasecheck ==="" || lowerCasecheck === null){
+
+      window.alert("That is not a  valid option. Please start again")
+      
+      password()
+      return;}
+
+    if(upCasecheck === "y"){
+
+      lowercase=true}
+
+
+    if(upCasecheck === "n"){
+
+      uppercase=false;}
+
+    
+    var  numCheck=window.prompt("Would you like to password to include numbers? Y for Yes, N for No")
+
+    numCheck= numCheck.toLowerCase()
+
+    if(numCheck ==="" || numCheck ===null){
+
+      window.alert("That is not a  valid option. Please start again")
+      
+      password()
+      return;}
+
+
+    if(numCheck === "y"){
+
+      numbers=true;}
+
+    if(numCheck === "n"){
+
+      numbers=false;} 
+
+    var symbolCheck =window.prompt("Would you like to include Symbols in your password? Y for Yes N for N")
+
+      symbolCheck= symbolCheck.toLowerCase()
+
+      if(symbolCheck === "" || symbolCheck === null){
+
+      window.alert("That is not a  valid option. Please start again")
+      
+      password()
+      return;}
+      
+    if(symbolCheck === "y"){
+
+      symbol=true;}
+
+    if(symbolCheck === "n"){
+
+      symbol=false;}
+
+
+    var truestring =[]
+
+      truestring.push(lengthChoice +" Characters long")
+
+      if(lowercase === true){
+
+        truestring.push(" Lowercase")}
+
+      if(uppercase === true){
+
+        truestring.push(" Uppercase")
+      }
+
+      if(numbers === true){
+
+        truestring.push(" Numbers")
+      }
+
+      if(symbol === true){
+
+        truestring.push(" symbols")
+      }
+
+      var confirmChoice = truestring.toString()
+
+    var Choice =window.confirm("Are you sure you would like to add the following to your customer password? " +truestring)
+
+      if(choice === true){}
+  }
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* old instructions
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+/// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -14,4 +161,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); */
